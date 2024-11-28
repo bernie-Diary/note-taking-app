cc722e598693412a3a73ace5b7a5181d77121447
bernice <bernyx.owusu@gmail.com>
2024-11-28 15:51:05 +0000

    clerk added

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+    return (
+        //center the sign in from clerk 
+        <div className='flex justify-center items-center h-[100vh] '>
+         <SignIn />
+      </div>
+      )
+}
