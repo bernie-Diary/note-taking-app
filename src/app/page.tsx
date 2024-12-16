78799556265dd7701b7382369e9a7e953f92de2f
bernice <bernyx.owusu@gmail.com>
2024-12-16 10:57:59 +0000

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import TypewriterTitle from "@/components/TypewriterTitle";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <Button>Click </Button>
+    <div className="bg-gradient-to-r min-h-screen from-rose-100 to-teal-100 grainy">
+          <div className='flex flex-col justify-center items-center h-[100vh] '>
+            <h1 className="font-semibold text-7xl text-center">
+              AI <span className="text-green-600 font-bold">note taking</span> assistant.
+            </h1>
+
+            <div className="mt-4">
+              <h2 className="font-semibold text-3xl text-center text-slate-700">
+                <TypewriterTitle/>
+              </h2>
+            </div>
+            <div className="mt-8">
+                <Link href="/dashboard">
+                  <Button className="bg-green-600"> Get Started <ArrowRight className="ml-1 w-5 h-5"/></Button>
+                </Link>
+              </div>
+          </div>
+    </div>
   );
 }
